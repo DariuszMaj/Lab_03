494e6dab902c13a1c1d7f3e9adfdaacf6dde1cb2
DariuszMaj <91788770+DariuszMaj@users.noreply.github.com>
2022-04-02 16:10:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Lab_03.Logger;
+namespace Lab_03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string host = "google.com";
+            int port = 80;
+
+            using (ClientSocket clientSocket = new ClientSocket(host, port))
+            {
+                // request:
+
+                string requestText = "Message to sent ...";
+                byte[] requestBytes = Encoding.UTF8.GetBytes(requestText);
+
+                clientSocket.Send(requestBytes);
+
+                // response:
+
+                byte[] responseBuffer = new byte[1024];
+                int responseSize = clientSocket.Receive(responseBuffer);
+
+                string responseText = Encoding.UTF8.GetString(responseBuffer, 0, responseSize); // received message
+
+                // ...
+
+                // cleaning:
+
+                clientSocket.Close();
+            }
+        }
+    }
+}
---
+++ WriterLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03.Logger
+{
+    public abstract class WriterLogger : ILogger
+    {
+        protected TextWriter writer;
+
+        public virtual void Log(params string[] messages)
+        {
+            // Uzupełnić to miejsce o logikę zapisu opartą o TextWriter ...
+        }
+
+        public abstract void Dispose();
+    }
+}
